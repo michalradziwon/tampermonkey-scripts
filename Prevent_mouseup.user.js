a572d6f60f94930ce9ff2224e707b52a1889ad4c
Michal <michal.radzi.won@gmail.com>
2014-12-05 12:54:14 +0000

    init

---
+++ Prevent_mouseup.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Prevent 'mouseup'
+// @namespace    https://github.com/michalradziwon
+// @version      0.1
+// @description  Stops propagation of 'mouseup' event. I found it usefull when developing reactjs app and onClick handler was executed twice (for mouseup and click event). It could be use as a temporary solution.
+// @author       michalradziwon
+// @match        http://localhost:9000/*
+// @grant        none
+// ==/UserScript==
+
+
+console.log("STARTING_ ");
+
+document.addEventListener("mouseup", function (e) {
+  console.log(e);
+  e.stopPropagation();
+}, true);
